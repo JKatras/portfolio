db398047eaf4082e8862a0156cd14783905335dc
Jeremy Katras <jkatras@fullsail.edu>
2014-06-30 14:09:34 -0400

    added carousel/controls
    -rudimentary carousel slideshow in place
    -need height/width adjustments

---
+++ assets/scripts/js/main.js
@@ -0,0 +1,52 @@
+//jQuery(document).ready(function(){
+//	$(function() {
+//	    $('.jcarousel').jcarousel();
+//	    
+//	    $('.jcarousel-control-prev')
+//	            .on('jcarouselcontrol:active', function() {
+//	                $(this).removeClass('inactive');
+//	            })
+//	            .on('jcarouselcontrol:inactive', function() {
+//	                $(this).addClass('inactive');
+//	            })
+//	            .jcarouselControl({
+//	                target: '-=1'
+//	            });
+//	
+//        $('.jcarousel-control-next')
+//            .on('jcarouselcontrol:active', function() {
+//                $(this).removeClass('inactive');
+//            })
+//            .on('jcarouselcontrol:inactive', function() {
+//                $(this).addClass('inactive');
+//            })
+//            .jcarouselControl({
+//                target: '+=1'
+//            });
+//	});
+//});
+$(document).ready(function() {
+	$('.jcarousel').jcarousel();
+	    
+    $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+    $('.jcarousel-control-next')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '+=1'
+        });
+});
