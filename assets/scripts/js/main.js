bca693539bbcd32ad113fd56295df97edd17f46e
Jeremy Katras <jkatras@fullsail.edu>
2014-07-11 20:55:38 -0400

    new carousel. FML, not responsive (no idea why not)

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -1,19 +1,7 @@
-//jQuery(document).ready(function(){
-//	$(function() {
-//	    $('.jcarousel').jcarousel();
+//$(document).ready(function() {
+//	$('.jcarousel').jcarousel();
 //	    
-//	    $('.jcarousel-control-prev')
-//	            .on('jcarouselcontrol:active', function() {
-//	                $(this).removeClass('inactive');
-//	            })
-//	            .on('jcarouselcontrol:inactive', function() {
-//	                $(this).addClass('inactive');
-//	            })
-//	            .jcarouselControl({
-//	                target: '-=1'
-//	            });
-//	
-//        $('.jcarousel-control-next')
+//    $('.jcarousel-control-prev')
 //            .on('jcarouselcontrol:active', function() {
 //                $(this).removeClass('inactive');
 //            })
@@ -21,32 +9,28 @@
 //                $(this).addClass('inactive');
 //            })
 //            .jcarouselControl({
-//                target: '+=1'
+//                target: '-=1'
 //            });
-//	});
+//
+//    $('.jcarousel-control-next')
+//        .on('jcarouselcontrol:active', function() {
+//            $(this).removeClass('inactive');
+//        })
+//        .on('jcarouselcontrol:inactive', function() {
+//            $(this).addClass('inactive');
+//        })
+//        .jcarouselControl({
+//            target: '+=1'
+//        });
 //});
-$(document).ready(function() {
-	$('.jcarousel').jcarousel();
-	    
-    $('.jcarousel-control-prev')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .jcarouselControl({
-                target: '-=1'
-            });
 
-    $('.jcarousel-control-next')
-        .on('jcarouselcontrol:active', function() {
-            $(this).removeClass('inactive');
-        })
-        .on('jcarouselcontrol:inactive', function() {
-            $(this).addClass('inactive');
-        })
-        .jcarouselControl({
-            target: '+=1'
-        });
+$(document).ready(function() {
+	$('.bxslider').bxSlider({
+		minSlides: 2,
+		maxSlides: 2,
+		slideWidth: 800,
+		slideMargin: 10,
+//		adaptiveHeight: true,
+		mode: 'fade'
+	});
 });
