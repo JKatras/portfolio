0d3d7ddb69df135ad82d733adf055f5f92c4eb35
Jeremy Katras <jkatras@fullsail.edu>
2014-07-16 12:34:15 -0400

    sticky nav, scroll animations, cleanup

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -1,50 +1,27 @@
-//$(document).ready(function() {
-//	$('.jcarousel').jcarousel();
-//	    
-//    $('.jcarousel-control-prev')
-//            .on('jcarouselcontrol:active', function() {
-//                $(this).removeClass('inactive');
-//            })
-//            .on('jcarouselcontrol:inactive', function() {
-//                $(this).addClass('inactive');
-//            })
-//            .jcarouselControl({
-//                target: '-=1'
-//            });
-//
-//    $('.jcarousel-control-next')
-//        .on('jcarouselcontrol:active', function() {
-//            $(this).removeClass('inactive');
-//        })
-//        .on('jcarouselcontrol:inactive', function() {
-//            $(this).addClass('inactive');
-//        })
-//        .jcarouselControl({
-//            target: '+=1'
-//        });
-//});
-
-//$(document).ready(function() {
-//	$('.bxslider').bxSlider({
-//		minSlides: 2,
-//		maxSlides: 2,
-//		slideWidth: 800,
-//		slideMargin: 10,
-//		adaptiveHeight: true,
-//		mode: 'fade'
-//	});
-//});
-
 $(document).ready(function () {
 	$('.flexslider').flexslider({
-//		itemWidth: 500,
-//		itemMargin: 10,
 		animation: "slide",
 		slideshow: false,
 		directionNav: true,
 		controlNav: false,
-//		minItems: 1,
-//		maxItems: 2,
 		touch: true
 	});
+	$("#scroll-skills").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top - 30
+        }, 800);
+        return false;
+    });
+    $("#scroll-projects").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top - 30
+        }, 800);
+        return false;
+    });
+    $("#scroll-contact").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top - 30
+        }, 800);
+        return false;
+    });
 });
